bcbf421fb76fb5f606ca61ae4a5edfddf029ae7c
Loris-R <loris.rynaud@gmail.com>
2023-11-28 17:57:22 +0100

    final commit

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,10 @@
 class Pet < ApplicationRecord
+  SPECIES = ["cat", "dog", "turtle", "snake"]
+
   validates :name, :address, presence: true
-  validates :species, inclusion: { in: ["chat", "chien", "tortue", "serpent"] }
+  validates :species, inclusion: { in: SPECIES }
+
+  def found_since
+    (Date.today - found_on).to_i
+  end
 end
